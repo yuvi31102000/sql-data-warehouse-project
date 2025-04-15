6e1d8952f5f006611e7ddce705bd7a0c0e235b4a
yuvi31102000 <109458610+yuvi31102000@users.noreply.github.com>
2025-04-15 13:20:16 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,17 @@
+
+/* Create database and schemas */
+
+USE Master;
+
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+
+-- Create Schemas
+
+CREATE SCHEMA bronze;
+
+CREATE SCHEMA silver;
+
+CREATE SCHEMA gold;
